d74899d309b472f2fc8a7ee1205ceb43dc4f109e
bytes <54559056+motibytes@users.noreply.github.com>
2021-11-19 10:23:05 -0500

    fire

---
+++ my-app/src/App.js
@@ -0,0 +1,63 @@
+import logo from './logoforicons.jpg';
+import apple from './apple.svg';
+import apple2 from './apple2.svg';
+
+import amazon from './amazonh.png';
+import amazon2 from './amazonh2.png';
+import amazon3 from './amazonh3.png';
+import amazon4 from './amazonh4.png';
+import amazon5 from './amazonh5.png';
+import amazon6 from './amazonh6.png';
+import amazon7 from './amazonh7.png';
+import amazon8 from './amazonh8.png';
+import gates from './gates.jpg';
+
+import './App.css';
+
+import Share from './Share.js';
+function App() {
+  return (
+    <div className="App">
+
+      <header className="App-header">
+      TheGatesoftheSoulOpen.com
+      <img src={logo} className="App-logo" alt="logo" />
+
+      Spiritual Warfare: Gates of the Soul
+
+
+      </header>
+      <div id="main">
+                
+        <p id="readthescroll">
+        Read the scroll. Overcome through Yeshua. Remember His Word.
+        </p>
+      </div>  
+      <div id="links"> 
+        <a id="a_amazon" href="https://www.amazon.com/Spiritual-Warfare-Gates-Zerubbabel-Shaliach-ebook/dp/B09KN8754N">
+          <img src={amazon5} id="amazon" alt="logo" 
+          />
+        </a>
+        <br />
+        <a id="a_apple" href="https://books.apple.com/us/book/spiritual-warfare/id1593748681">
+          <img src={apple2} id="apple" alt="logo" />
+        </a>
+        <br /> 
+        <a id="a_google" href='https://play.google.com/store/books/details/Zerubbabel_Shaliach_Spiritual_Warfare?id=eN5KEAAAQBAJ'><img id="icons" alt='Get it on Google Play' src='https://play.google.com/intl/en_us/badges/static/images/badges/en_badge_web_generic.png'/></a>
+
+      </div>
+      <br /> 
+      <Share />
+      <br /> 
+      <div id="about"> 
+        <h1> About the Author: </h1>
+        <p id="about"> A descendant of Judah, Zerubbabel Shaliach has been saved by the shed blood of the only Messiah, Yeshua HaMashiach, Jesus Christ.</p>
+      </div>
+      <div id="footer"> 
+        <p> Published by © ZShaliach LLC 2021. All Rights Reserved.</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/Share.js
@@ -0,0 +1,131 @@
+import {
+  EmailShareButton,
+  FacebookShareButton,
+  FacebookMessengerShareButton,
+  HatenaShareButton,
+  InstapaperShareButton,
+  LineShareButton,
+  LinkedinShareButton,
+  LivejournalShareButton,
+  MailruShareButton,
+  OKShareButton,
+  PinterestShareButton,
+  PocketShareButton,
+  RedditShareButton,
+  TelegramShareButton,
+  TumblrShareButton,
+  TwitterShareButton,
+  ViberShareButton,
+  VKShareButton,
+  WhatsappShareButton,
+  WorkplaceShareButton
+} from "react-share";
+
+//icons 
+import {
+    EmailIcon,
+    FacebookIcon,
+    FacebookMessengerIcon,
+    HatenaIcon,
+    InstapaperIcon,
+    LineIcon,
+    LinkedinIcon,
+    LivejournalIcon,
+    MailruIcon,
+    OKIcon,
+    PinterestIcon,
+    PocketIcon,
+    RedditIcon,
+    TelegramIcon,
+    TumblrIcon,
+    TwitterIcon,
+    ViberIcon,
+    VKIcon,
+    WeiboIcon,
+    WhatsappIcon,
+    WorkplaceIcon
+} from "react-share";
+import logo from './logo.jpg';
+
+let shareUrl = "https://thegatesofthesoulopen.com";
+let title = "I'm reading Gates of the Soul!"
+const Share = () => {
+    return ( 
+        <div>
+          <FacebookShareButton
+            url={shareUrl}
+            quote={title}
+            className="Demo__some-network__share-button"
+          >
+            <FacebookIcon size={32} round />
+          </FacebookShareButton>
+          <FacebookMessengerShareButton
+            url={shareUrl}
+            appId="521270401588372"
+            className="Demo__some-network__share-button"
+          >
+            <FacebookMessengerIcon size={32} round />
+          </FacebookMessengerShareButton>
+
+          <TelegramShareButton
+            url={shareUrl}
+            title={title}
+            className="Demo__some-network__share-button"
+          >
+            <TelegramIcon size={32} round />
+          </TelegramShareButton>
+          <WhatsappShareButton
+            url={shareUrl}
+            title={title}
+            separator=":: "
+            className="Demo__some-network__share-button"
+          >
+            <WhatsappIcon size={32} round />
+          </WhatsappShareButton>
+          <LinkedinShareButton url={shareUrl} className="Demo__some-network__share-button">
+            <LinkedinIcon size={32} round />
+          </LinkedinShareButton>
+          <PinterestShareButton
+            url={String(window.location)}
+            media={`${String(window.location)}/${logo}`}
+            className="Demo__some-network__share-button"
+          >
+            <PinterestIcon size={32} round />
+          </PinterestShareButton>
+          <RedditShareButton
+            url={shareUrl}
+            title={title}
+            windowWidth={660}
+            windowHeight={460}
+            className="Demo__some-network__share-button"
+          >
+            <RedditIcon size={32} round />
+          </RedditShareButton>
+          <TumblrShareButton
+            url={shareUrl}
+            title={title}
+            className="Demo__some-network__share-button"
+          >
+            <TumblrIcon size={32} round />
+          </TumblrShareButton>
+          <EmailShareButton
+            url={shareUrl}
+            subject={title}
+            body="body"
+            className="Demo__some-network__share-button"
+          >
+            <EmailIcon size={32} round />
+          </EmailShareButton>          
+          <TwitterShareButton
+            url={shareUrl}
+            title={title}
+            className="Demo__some-network__share-button"
+          >
+            <TwitterIcon size={32} round />
+          </TwitterShareButton>
+          <br /> 
+        </div>
+     );
+}
+ 
+export default Share;
